2aae189225f88eef9a5a50b02e2edad1635dd8d4
kristupas <kristupas.liubinas@gmail.com>
2020-03-11 15:49:23 +0000

    Initial commit

---
+++ app/src/main/java/com/example/whisper_wearos/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.whisper_wearos;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+
+public class MainActivity extends WearableActivity {
+
+    private Button notificationButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //get access to views defined in layout
+        notificationButton = findViewById(R.id.notificationButton);
+
+        //specify what happens when notificationButton is pressed
+        notificationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNotification();
+            }
+        });
+    }
+
+    private void addNotification () {
+        //build the notification
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Whisper Reply")
+                .setContentText("I agree!");
+
+        //create the intent needed to show the notification
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this,0, notificationIntent, 0);
+        notificationBuilder.setContentIntent(contentIntent);
+
+        //add a notification
+        NotificationManagerCompat manager = NotificationManagerCompat.from(this);
+        manager.notify(0, notificationBuilder.build());
+    }
+}
