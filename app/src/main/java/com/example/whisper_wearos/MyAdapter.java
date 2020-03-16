52d90d1917ac4b667334e2cc391e53bd8e24f97c
kristupas <kristupas.liubinas@gmail.com>
2020-03-16 19:12:01 +0000

    Added the Action Drawer with dummy actions on the Reply View

---
+++ app/src/main/java/com/example/whisper_wearos/MyAdapter.java
@@ -0,0 +1,41 @@
+package com.example.whisper_wearos;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class MyAdapter extends ArrayAdapter<String> {
+
+    Context context;
+    String rTitle[];
+    int rImgs[];
+
+    public MyAdapter (Context c, String title[], int imgs[]) {
+        super(c, R.layout.row, R.id.actionText, title);
+        this.context = c;
+        this.rTitle = title;
+        this.rImgs = imgs;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater layoutInflater = (LayoutInflater) context.getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View row = layoutInflater.inflate(R.layout.row, parent, false);
+        ImageView images = row.findViewById(R.id.actionImage);
+        TextView myActionText = row.findViewById(R.id.actionText);
+
+        // set resources on views
+        images.setImageResource(rImgs[position]);
+        myActionText.setText(rTitle[position]);
+
+        return row;
+    }
+}
--- app/src/main/java/com/example/whisper_wearos/ReplyActivity.java
+++ app/src/main/java/com/example/whisper_wearos/ReplyActivity.java
@@ -2,18 +2,47 @@ package com.example.whisper_wearos;
 
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.wear.widget.drawer.WearableDrawerView;
 
 public class ReplyActivity extends WearableActivity {
 
-    private TextView messageTextView;
+    private ListView listView;
+    String mTitle[] = {"Like", "Reply", "Chat"};
+    int images[] = {R.drawable.ic_m_white_48dp, R.drawable.ic_m_white_48dp, R.drawable.ic_m_white_48dp};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reply);
+        setContentView(R.layout.activity_reply3);
 
-        //get access to views defined in layout
-        messageTextView = findViewById(R.id.messageTextView);
+        listView = findViewById(R.id.actionDrawer);
+
+        // create an adapter class --> MyAdapter.java
+
+        MyAdapter adapter = new MyAdapter(this, mTitle, images);
+        listView.setAdapter(adapter);
+
+        // set item click on list view
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    Toast.makeText(ReplyActivity.this, "You have liked", Toast.LENGTH_SHORT).show();
+                }
+                if (position == 1) {
+                    Toast.makeText(ReplyActivity.this, "You have replied", Toast.LENGTH_SHORT).show();
+                }
+                if (position == 2) {
+                    Toast.makeText(ReplyActivity.this, "Start a chat", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
 }
